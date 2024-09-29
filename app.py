03f47a67707b94db9509f507da22214fe8d84102
Roger Taylor <rogermt@users.noreply.github.com>
2024-09-29 12:00:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 SYSTEM_MESSAGE = """You are a bot that is very good at solving puzzles. You will work with the user who will present you a new puzzle to solve it.
 The puzzle consists of a list of EXAMPLES, each containing an INPUT/OUTPUT pair describing a pattern which is shared amongst all examples.
 The user will also provide a TEST INPUT for which you are to produce a predicted OUTPUT that follows the common pattern of all the examples.
